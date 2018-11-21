93ef0c9c44b62ef4d7367a4a8afa61287d0536c4
623nana <6230nana@gmail.com>
2018-11-21 22:44:06 +0900

    Edit homework

--- hw/HW3Student20141051.py
+++ hw/HW3Student20141051.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+
 import urllib.request
 import json
 from datetime import datetime
 from hw import configuration, mysqldb
 import threading
+import sys
+
+# config.props
+p = sys.argv[1]
 
 def insert(ccn, tcn, price, conn):
     cursor = conn.cursor()
@@ -22,9 +28,10 @@ def task():
     eth_list = json.loads(eth.read().decode('utf8').replace("'", '"'))
 
     # db connection
-    config = configuration.get_db_configuration('config.props')
+    config = configuration.get_db_configuration(p)
     c = mysqldb.get_connection(config)
 
+    # 한번씩 커밋이 다 필요할까..?
     for i in btc_list:
         insert('BTC', i, btc_list[i], c)
         c.commit()
@@ -32,8 +39,9 @@ def task():
     for i in eth_list:
         insert('ETH', i, eth_list[i], c)
         c.commit()
-
     c.close()
+
+    # 10분마다 실행
     threading.Timer(600, task).start()
 
 def main():
